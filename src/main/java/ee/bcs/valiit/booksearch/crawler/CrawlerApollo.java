cc9927b83d3198e7d774320d0aaf9c0de15521d5
Kaie Palmaru <kaiehe@yahoo.com>
2021-05-05 16:49:25 +0300

    Apollo webcrawler - first page crawling works

--- src/main/java/ee/bcs/valiit/booksearch/crawler/CrawlerApollo.java
+++ src/main/java/ee/bcs/valiit/booksearch/crawler/CrawlerApollo.java
@@ -8,18 +8,17 @@ import org.jsoup.select.Elements;
 public class CrawlerApollo {
     public static void main(String[] args) {
         String contents = WebReader.readWeb("https://www.apollo.ee/raamatud/eestikeelsed-raamatud/uued-eesti-keeles");
-        //System.out.println(contents2);
 
         Document document = Jsoup.parse(contents);
-       // System.out.println(document);
-        Elements elements = document.select("div.products-grid.item");
+        Elements elements = document.select("div.products-grid .item");
         System.out.println(elements.size());
         for (Element e : elements) {
-            //System.out.println(e.attributes().get("href"));
-            //System.out.println(e.text());
             Elements productName = e.getElementsByClass("product-name");
             String title = productName.get(0).getElementsByTag("a").get(0).attributes().get("title");
-            System.out.println(title);
+            String link = productName.get(0).getElementsByTag("a").get(0).attributes().get("href");
+            String productPrice = e.getElementsByClass("regular-price").text();
+            String author = e.getElementsByClass("author").text();
+            System.out.println(title + " " + " " + author + " " + productPrice + " " + link);
         }
     }
 }
