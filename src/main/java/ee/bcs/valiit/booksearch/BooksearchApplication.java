7da7068a0ac8e0a794971e7b701e0e579d89199d
Birgit Plakso <birgit.plakso@gmail.com>
2021-05-12 15:04:50 +0300

    12.05.21 @EnableScheduling: @Scheduled - meetod tehtud(24h)

--- src/main/java/ee/bcs/valiit/booksearch/BooksearchApplication.java
+++ src/main/java/ee/bcs/valiit/booksearch/BooksearchApplication.java
@@ -2,7 +2,9 @@ package ee.bcs.valiit.booksearch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class BooksearchApplication {
 
